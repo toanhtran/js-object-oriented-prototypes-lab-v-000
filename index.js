56a5cf8855ed906d5f86e380b3d57ae7bea03d78
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-05 01:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ BoardMember.prototype.approve = function(){
     return "You will see great things from Scuber."
   }
 
-  this.sayHi = function(){
+BoardMember.prototype.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
